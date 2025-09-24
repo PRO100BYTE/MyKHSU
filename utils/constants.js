84ff2350916ab23adc6cbc6021281b358d2ab670
Evgeniy Struchkov <thedayg0ne@internet.ru>
2025-09-24 10:14:47 +0700

    Update application info
    Co-Authored-By: PRO100BYTE Helper <helper@pro100byte.ru>

--- utils/constants.js
+++ utils/constants.js
@@ -23,11 +23,11 @@ export const ACCENT_COLORS = {
 };
 
 // Версия приложения
-export const APP_VERSION = '2.0.6';
+export const APP_VERSION = '2.0.7';
 export const APP_DEVELOPERS = 'студентами группы 125-1 в составе команды PRO100BYTE Team';
 export const APP_SUPPORTERS = 'ХГУ им. Н.Ф. Катанова и ООО "Скалк Софт"';
-export const BUILD_VER = 'git-7abf2f8';
-export const BUILD_DATE = '23.09.2025';
+export const BUILD_VER = 'git-bcb9522';
+export const BUILD_DATE = '24.09.2025';
 
 // Дни недели
 export const WEEKDAYS = [
