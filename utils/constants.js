262f255f3c879be2c7dcd73826cbadc8dda0c5ab
Evgeniy Struchkov <thedayg0ne@internet.ru>
2025-09-25 22:56:52 +0700

    Increase app version
    Co-Authored-By: PRO100BYTE Helper <helper@pro100byte.ru>
    Co-Authored-By: rm30303 <89103543+rm30303@users.noreply.github.com>

--- utils/constants.js
+++ utils/constants.js
@@ -23,10 +23,10 @@ export const ACCENT_COLORS = {
 };
 
 // Версия приложения
-export const APP_VERSION = '2.1.2';
+export const APP_VERSION = '2.1.3';
 export const APP_DEVELOPERS = 'студентами группы 125-1 в составе команды PRO100BYTE Team';
 export const APP_SUPPORTERS = 'ХГУ им. Н.Ф. Катанова и ООО "Скалк Софт"';
-export const BUILD_VER = 'git-b9ab350';
+export const BUILD_VER = 'git-7056312';
 export const BUILD_DATE = '25.09.2025';
 
 // Дни недели
