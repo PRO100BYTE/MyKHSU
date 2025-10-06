a362405ca4df5c3462c0344f4188898e6eb29eeb
Evgeniy Struchkov <thedayg0ne@internet.ru>
2025-10-06 21:19:28 +0700

    Update Splash Screen
    Co-Authored-By: PRO100BYTE Helper <helper@pro100byte.ru>

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -5,15 +5,16 @@ import { ACCENT_COLORS } from '../utils/constants';
 
 const SplashScreen = ({ accentColor, theme }) => {
   const colors = ACCENT_COLORS[accentColor];
-  
-  // Определяем цвет фона в зависимости от темы
   const backgroundColor = theme === 'dark' ? '#111827' : '#f3f4f6';
-  // Определяем цвет иконки и текста в зависимости от темы
   const iconColor = theme === 'dark' ? colors.light : colors.primary;
   const textColor = theme === 'dark' ? '#ffffff' : colors.primary;
   
   return (
     <View style={[styles.flexCenter, { backgroundColor }]}>
+      <StatusBar 
+        barStyle={theme === 'light' ? 'dark-content' : 'light-content'}
+        backgroundColor={backgroundColor}
+      />
       <Icon name="school-outline" size={120} color={iconColor} />
       <Text style={[styles.title, { color: textColor, marginTop: 20, fontFamily: 'Montserrat_700Bold' }]}>
         Мой ИТИ ХГУ
