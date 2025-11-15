49ac7cea434b97e5373f44708feeb67fe1f4f721
Evgeniy Struchkov <thedayg0ne@internet.ru>
2025-11-16 00:00:12 +0700

    Update building coordinates
    Co-Authored-By: PRO100BYTE Helper <helper@pro100byte.ru>

--- utils/buildingCoordinates.js
+++ utils/buildingCoordinates.js
@@ -1,51 +1,323 @@
 export const buildings = [
   {
     id: 1,
-    name: 'Корпус №2 (ИТИ)',
-    latitude: 53.722143,
-    longitude: 91.439183,
-    description: 'ул. Ленина, 92/1',
-    type: 'academic'
-  },
-  {
-    id: 2,
     name: 'Административный корпус',
-    latitude: 53.722127,
-    longitude: 91.438486,
-    description: 'ул. Ленина, 92',
+    latitude: 53.722052,
+    longitude: 91.438492,
+    description: 'ул. Ленина, 92/1',
     type: 'main'
   },
   {
-    id: 3,
+    id: 2,
     name: 'Корпус №1 (ИЕНиМ)',
-    latitude: 53.722481,
-    longitude: 91.441737,
+    latitude: 53.722388,
+    longitude: 91.441705,
     description: 'ул. Ленина, 90',
     type: 'academic'
   },
   {
+    id: 3,
+    name: 'Библиотека (ИЕНиМ)',
+    latitude: 53.722695,
+    longitude: 91.441165,
+    description: 'ул. Ленина, 90',
+    type: 'library'
+  },
+  {
     id: 4,
-    name: 'Корпус №3 (ИФИиМК)',
-    latitude: 53.7215,
-    longitude: 91.4385,
-    description: 'ул. Ленина, 94',
-    type: 'academic'
+    name: 'Столовая / Буфет (ИЕНиМ)',
+    latitude: 53.722770,
+    longitude: 91.442020,
+    description: 'ул. Ленина, 90',
+    type: 'cafeteria'
   },
   {
     id: 5,
-    name: 'Спортивный комплекс',
-    latitude: 53.7210,
-    longitude: 91.4370,
-    description: 'ул. Ленина, 96',
+    name: 'Спортплощадка (ИЕНиМ)',
+    latitude: 53.722822,
+    longitude: 91.441618,
+    description: 'ул. Ленина, 90',
     type: 'sports'
   },
   {
     id: 6,
-    name: 'Библиотека',
-    latitude: 53.7220,
-    longitude: 91.4400,
-    description: 'ул. Ленина, 92',
+    name: 'Банкомат (ИЕНиМ)',
+    latitude: 53.722352,
+    longitude: 91.441093,
+    description: 'ул. Ленина, 90',
+    type: 'cardatm'
+  },
+  {
+    id: 7,
+    name: 'Корпус №2 (ИТИ, ИИП, МПСИ)',
+    latitude: 53.722039,
+    longitude: 91.439316,
+    description: 'ул. Ленина, 92/1',
+    type: 'academic'
+  },
+  {
+    id: 8,
+    name: 'Библиотека (ИТИ, ИИП, МПСИ)',
+    latitude: 53.722202,
+    longitude: 91.439218,
+    description: 'ул. Ленина, 92/1',
     type: 'library'
+  },
+  {
+    id: 9,
+    name: 'Столовая / Буфет (ИТИ, ИИП, МПСИ)',
+    latitude: 53.722062,
+    longitude: 91.439009,
+    description: 'ул. Ленина, 92/1',
+    type: 'cafeteria'
+  },
+  {
+    id: 10,
+    name: 'Корпус №3 (ИФиМК, ИНПО)',
+    latitude: 53.722015,
+    longitude: 91.437456,
+    description: 'ул. Ленина, 92/5',
+    type: 'academic'
+  },
+  {
+    id: 11,
+    name: 'Библиотека (ИФиМК, ИНПО)',
+    latitude: 53.722012,
+    longitude: 91.437090,
+    description: 'ул. Ленина, 92/5',
+    type: 'library'
+  },
+  {
+    id: 12,
+    name: 'Столовая (ИФиМК, ИНПО)',
+    latitude: 53.722209,
+    longitude: 91.437780,
+    description: 'ул. Ленина, 92/5',
+    type: 'cafeteria'
+  },
+  {
+    id: 13,
+    name: 'Корпус №4 (ИЭУ)',
+    latitude: 53.719106,
+    longitude: 91.439870,
+    description: 'ул. Карла Маркса, 11',
+    type: 'academic'
+  },
+  {
+    id: 14,
+    name: 'Корпус №8 (СХК, СХИ)',
+    latitude: 53.722366,
+    longitude: 91.468582,
+    description: 'ул. Хакасская, 6',
+    type: 'academic'
+  },
+  {
+    id: 15,
+    name: 'Банкомат (СХК, СХИ)',
+    latitude: 53.722568,
+    longitude: 91.468219,
+    description: 'ул. Хакасская, 6',
+    type: 'cardatm'
+  },
+  {
+    id: 16,
+    name: 'Библиотека (СХК, СХИ)',
+    latitude: 53.722534,
+    longitude: 91.469040,
+    description: 'ул. Хакасская, 6',
+    type: 'library'
+  },
+  {
+    id: 17,
+    name: 'Корпус №9-11 (ИМЭА)',
+    latitude: 53.721150,
+    longitude: 91.468448,
+    description: 'ул. Хакасская, 6',
+    type: 'academic'
+  },
+  {
+    id: 18,
+    name: 'Корпус №13 (легкоатлетический манеж)',
+    latitude: 53.721654,
+    longitude: 91.469443,
+    description: 'ул. Хакасская, 6',
+    type: 'sports'
+  },
+  {
+    id: 19,
+    name: 'Корпус №15 (КПОИиП)',
+    latitude: 53.719940,
+    longitude: 91.443613,
+    description: 'ул. Щетинкина, 13',
+    type: 'academic'
+  },
+  {
+    id: 20,
+    name: 'Столовая (КПОИиП)',
+    latitude: 53.719931,
+    longitude: 91.444053,
+    description: 'ул. Щетинкина, 13',
+    type: 'cafeteria'
+  },
+  {
+    id: 21,
+    name: 'Библиотека (КПОИиП)',
+    latitude: 53.719851,
+    longitude: 91.443256,
+    description: 'ул. Щетинкина, 13',
+    type: 'library'
+  },
+  {
+    id: 22,
+    name: 'Корпус №16 (Мед. колледж)',
+    latitude: 53.719820,
+    longitude: 91.441838,
+    description: 'ул. Хакасская, 68',
+    type: 'academic'
+  },
+  {
+    id: 23,
+    name: 'Столовая (Мед. колледж)',
+    latitude: 53.719780,
+    longitude: 91.442430,
+    description: 'ул. Хакасская, 68',
+    type: 'cafeteria'
+  },
+  {
+    id: 24,
+    name: 'Библиотека (Мед. колледж)',
+    latitude: 53.719609,
+    longitude: 91.441320,
+    description: 'ул. Хакасская, 68',
+    type: 'library'
+  },
+  {
+    id: 25,
+    name: 'Корпус №17 (ИИ, Муз. колледж)',
+    latitude: 53.724581,
+    longitude: 91.447360,
+    description: 'ул. Вяткина, 14',
+    type: 'academic'
+  },
+  {
+    id: 26,
+    name: 'Библиотека (ИИ, Муз. колледж)',
+    latitude: 53.724832,
+    longitude: 91.447304,
+    description: 'ул. Вяткина, 14',
+    type: 'library'
+  },
+  {
+    id: 27,
+    name: 'Банкомат (ИИ, Муз. колледж)',
+    latitude: 53.724237,
+    longitude: 91.447777,
+    description: 'ул. Вяткина, 14',
+    type: 'cardatm'
+  },
+  {
+    id: 28,
+    name: 'Университетский сквер',
+    latitude: 53.722436,
+    longitude: 91.440559,
+    description: 'между ИТИ и ИЕНиМ',
+    type: 'garden'
+  },
+  {
+    id: 29,
+    name: 'Общежитие №1',
+    latitude: 53.721755,
+    longitude: 91.436523,
+    description: 'ул. Ленина, 92/А1',
+    type: 'dormitory'
+  },
+  {
+    id: 30,
+    name: 'Общежитие №2',
+    latitude: 53.724689,
+    longitude: 91.439043,
+    description: 'ул. Чертыгашева, 118',
+    type: 'dormitory'
+  },
+  {
+    id: 31,
+    name: 'Общежитие №3',
+    latitude: 53.722379,
+    longitude: 91.469533,
+    description: 'ул. Хакасская, 6/А9',
+    type: 'dormitory'
+  },
+  {
+    id: 32,
+    name: 'Общежитие №4',
+    latitude: 53.722348,
+    longitude: 91.470480,
+    description: 'ул. Хакасская, 6/А10',
+    type: 'dormitory'
+  },
+  {
+    id: 33,
+    name: 'Общежитие №5',
+    latitude: 53.722378,
+    longitude: 91.471439,
+    description: 'ул. Хакасская, 6/А11',
+    type: 'dormitory'
+  },
+  {
+    id: 34,
+    name: 'Общежитие №7',
+    latitude: 53.719111,
+    longitude: 91.443654,
+    description: 'ул. Щетинкина, 11А',
+    type: 'dormitory'
+  },
+  {
+    id: 35,
+    name: 'Столовая (Общежитие №7)',
+    latitude: 53.719302,
+    longitude: 91.443261,
+    description: 'ул. Щетинкина, 11А',
+    type: 'cafeteria'
+  },
+  {
+    id: 36,
+    name: 'Общежитие №8',
+    latitude: 53.725036,
+    longitude: 91.447687,
+    description: 'ул. Чертыгашева, 65А/3',
+    type: 'dormitory'
+  },
+  {
+    id: 37,
+    name: 'Общежитие №9',
+    latitude: 53.717150,
+    longitude: 91.434797,
+    description: 'ул. Ярыгина, 14',
+    type: 'dormitory'
+  },
+  {
+    id: 38,
+    name: 'Общежитие №11',
+    latitude: 53.723289,
+    longitude: 91.441369,
+    description: 'ул. Ленина, 90А',
+    type: 'dormitory'
+  },
+  {
+    id: 39,
+    name: 'Пятерочка',
+    latitude: 53.723177,
+    longitude: 91.439694,
+    description: 'ул. Карла Маркса, 21',
+    type: '5ka'
+  },
+  {
+    id: 40,
+    name: 'Hot Dog Time',
+    latitude: 53.721050,
+    longitude: 91.440371,
+    description: 'ул. Советская, 30',
+    type: 'sausage'
   }
 ];
 
