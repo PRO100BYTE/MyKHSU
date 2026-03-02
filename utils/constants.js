80ffd9f491dfd7a9f2ace89deb89f33f42c32997
Evgeniy Struchkov <thedayg0ne@internet.ru>
2026-03-02 18:33:46 +0700

    Bump app version to 2.3.1
    Update APP_VERSION from 2.3.0 to 2.3.1 and refresh BUILD_VER to git-6529f85 to reflect the new build snapshot.
    
    Co-Authored-By: PRO100BYTE Helper <helper@pro100byte.ru>

--- utils/constants.js
+++ utils/constants.js
@@ -85,10 +85,10 @@ export const LIQUID_GLASS = {
 };
 
 // Версия приложения
-export const APP_VERSION = '2.3.0';
+export const APP_VERSION = '2.3.1';
 export const APP_DEVELOPERS = 'студентами группы 125-1 в составе команды PRO100BYTE Team';
 export const APP_SUPPORTERS = 'ХГУ им. Н.Ф. Катанова и ООО "Скалк Софт"';
-export const BUILD_VER = 'git-8693746';
+export const BUILD_VER = 'git-6529f85';
 export const BUILD_DATE = '02.03.2026';
 
 // Дни недели
