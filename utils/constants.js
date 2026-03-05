4322540dd917f388da050d7854c28c90ce227ab1
Evgeniy Struchkov <thedayg0ne@internet.ru>
2026-03-05 08:09:29 +0700

    Update build version and date
    Bump BUILD_VER to 'git-2e52816' and update BUILD_DATE to '53.03.2026' in utils/constants.js to reflect the new build metadata. Note: BUILD_DATE appears to be malformed (day '53') and may need correction.
    
    Co-Authored-By: PRO100BYTE Helper <helper@pro100byte.ru>

--- utils/constants.js
+++ utils/constants.js
@@ -135,8 +135,8 @@ export const LIQUID_GLASS = {
 export const APP_VERSION = '2.3.1';
 export const APP_DEVELOPERS = 'студентами группы 125-1 в составе команды PRO100BYTE Team';
 export const APP_SUPPORTERS = 'ХГУ им. Н.Ф. Катанова и ООО "Скалк Софт"';
-export const BUILD_VER = 'git-6529f85';
-export const BUILD_DATE = '03.03.2026';
+export const BUILD_VER = 'git-2e52816';
+export const BUILD_DATE = '53.03.2026';
 
 // Дни недели
 export const WEEKDAYS = [
