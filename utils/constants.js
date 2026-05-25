b33b62f1ef5c3ea6733b8a554f2770853325b882
Evgeniy Struchkov <thedayg0ne@internet.ru>
2026-05-25 10:36:52 +0700

    Bump BUILD_VER and BUILD_DATE
    Update build metadata in utils/constants.js: BUILD_VER changed from 'git-09f4be9' to 'git-0d44c62' and BUILD_DATE updated from '21.04.2026' to '25.05.2026' to reflect the new build.
    
    Co-Authored-By: PRO100BYTE Helper <helper@pro100byte.ru>

--- utils/constants.js
+++ utils/constants.js
@@ -171,8 +171,8 @@ export const LIQUID_GLASS = {
 export const APP_VERSION = '2.3.2';
 export const APP_DEVELOPERS = 'студентами группы 125-1 в составе команды PRO100BYTE Team';
 export const APP_SUPPORTERS = 'ХГУ им. Н.Ф. Катанова и ООО "Скалк Софт"';
-export const BUILD_VER = 'git-09f4be9';
-export const BUILD_DATE = '21.04.2026';
+export const BUILD_VER = 'git-0d44c62';
+export const BUILD_DATE = '25.05.2026';
 
 // Дни недели
 export const WEEKDAYS = [
